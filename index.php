a3000495144ea82a8efc7d29cac758ed90ae7f8d
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-26 16:18:02 +0100

    Creo il costruttore

--- index.php
+++ index.php
@@ -10,4 +10,15 @@ class persona{
     private $birthPlace;
     private $fiscalCode;
 
+    public function __construct($id, $name, $surname, $dateOfBirth, $birthPlace, $fiscalCode)
+    {
+        $this -> setId($Id);
+        $this -> setName($name);
+        $this -> setSurname($surname);
+        $this -> setDateOfBirth($dateOfBirth);
+        $this -> setBirthPlace($birthPlace);
+        $this -> setFiscalCode($fiscalCode);
+
+    }
+
 }
